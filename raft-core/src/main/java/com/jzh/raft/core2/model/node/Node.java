b381f6c1763af91d858eb491d664acd4535ff31c
zihanjiang <zihan.jiang@shopee.com>
2022-01-18 12:05:25 +0800

    core2 finish processReceiveRequestVoteRpc

--- raft-core/src/main/java/com/jzh/raft/core2/model/node/Node.java
+++ raft-core/src/main/java/com/jzh/raft/core2/model/node/Node.java
@@ -8,6 +8,8 @@ import com.jzh.raft.core2.model.rpc.msg.RequestVoteResult;
 import com.jzh.raft.core2.model.rpc.msg.RequestVoteRpc;
 import com.jzh.raft.core2.model.schedule.ElectionTimeoutSchedule;
 
+import java.util.Objects;
+
 public class Node implements INode {
     private String nodeId;
     private AbsNodeRole role;
@@ -57,7 +59,56 @@ public class Node implements INode {
      * @return
      */
     private RequestVoteResult processReceiveRequestVoteRpc(RequestVoteRpc requestVoteRpc) {
+        // when requestVote.term is bigger than currentTerm
+        // change to follower、update term、vote as appropriate
+        // if requestVote's committed log is newer than or equals to node's, vote to the requester
+        // first compare the term number, then compare the index
+        if (requestVoteRpc.getTerm() > this.role.getCurrentTerm()) {
+            String voteFor = null;
+            if (isCommittedLogNewerThanOwn(requestVoteRpc.getLastCommittedLogEntryTerm(), requestVoteRpc.getLastCommittedLastLogEntryIndex())) {
+                voteFor = requestVoteRpc.getNodeId();
+            }
+            this.role = new FollowerRole(requestVoteRpc.getTerm(), voteFor, getANewElectionTimeoutSchedule());
+            return new RequestVoteResult(this.nodeId, true, this.role.getCurrentTerm());
+        }
 
+        // when requestVote.term is equals to currentTerm
+        // follower: vote as appropriate, when don't vote to any node && requestVote's committed log is newer or equals than node's
+        // (follower get the requestVote of others but don't vote)
+        // candidate: don't vote
+        // leader: don't vote
+        if (Objects.equals(requestVoteRpc.getTerm(), this.role.getCurrentTerm())) {
+            switch (this.role.getRoleType()) {
+                case FOLLOWER:
+                    FollowerRole ownRole = (FollowerRole)this.role;
+                    if ((ownRole.getVoteFor() == null || Objects.equals(ownRole.getVoteFor(), requestVoteRpc.getNodeId())) &&
+                            isCommittedLogNewerThanOwn(requestVoteRpc.getLastCommittedLogEntryTerm(), requestVoteRpc.getLastCommittedLastLogEntryIndex())) {
+                        this.role = new FollowerRole(requestVoteRpc.getTerm(), requestVoteRpc.getNodeId(), getANewElectionTimeoutSchedule());
+                        return new RequestVoteResult(this.nodeId, true, this.role.getCurrentTerm());
+                    }
+                    return new RequestVoteResult(this.nodeId, false, this.role.getCurrentTerm());
+                case CANDIDATE:
+                case LEADER:
+                    return new RequestVoteResult(this.nodeId, false, this.role.getCurrentTerm());
+            }
+        }
+
+        return new RequestVoteResult(this.nodeId, false, this.role.getCurrentTerm());
+
+    }
+
+    private void onReceiveRequestVoteResult(RequestVoteResult requestVoteResult) {
+        this.nodeContext.getExecutorPool().submit(() -> processReceiveRequestVoteResult(requestVoteResult));
+    }
+
+    private void processReceiveRequestVoteResult(RequestVoteResult requestVoteResult) {
+
+    }
+
+    private Boolean isCommittedLogNewerThanOwn(Long lastCommittedLogTerm, Long lastCommittedLogIndex) {
+        return lastCommittedLogTerm > this.nodeContext.getLastCommitLogTerm() ||
+                (Objects.equals(lastCommittedLogTerm, this.nodeContext.getLastCommitLogTerm()) &&
+                        lastCommittedLogIndex >= this.nodeContext.getLastCommitLogIndex());
     }
 
 
--- raft-core/src/main/java/com/jzh/raft/core2/model/rpc/msg/RequestVoteResult.java
+++ raft-core/src/main/java/com/jzh/raft/core2/model/rpc/msg/RequestVoteResult.java
@@ -1,4 +1,19 @@
 package com.jzh.raft.core2.model.rpc.msg;
 
+import lombok.Getter;
+import lombok.NonNull;
+
 public class RequestVoteResult {
+    @Getter
+    private final String nodeId;
+    @Getter
+    private final Boolean result;
+    @Getter
+    private final Integer ownTerm;
+
+    public RequestVoteResult(@NonNull String nodeId, @NonNull Boolean result, @NonNull Integer ownTerm) {
+        this.nodeId = nodeId;
+        this.result = result;
+        this.ownTerm = ownTerm;
+    }
 }
